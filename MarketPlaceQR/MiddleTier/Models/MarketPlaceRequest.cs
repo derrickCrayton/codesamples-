3aa67bde78762f93b3cf73a208ff97af413ab7c7
Derrick Crayton <Derrick Crayton>
2017-03-20 08:46:47 -0700

    MarketPlaceQR Request Model

---
+++ MarketPlaceQR/MiddleTier/Models/MarketPlaceRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Models.Requests
+{
+    public class MarketPlaceRequest
+    {
+        public string Name { get; set; }
+
+        public string Estimation { get; set; }
+        
+        public string Status { get; set; }
+        
+        public string Address { get; set; }
+
+        public float LatPoint { get; set; }
+
+        public float LngPoint { get; set; }
+
+        public float Radius { get; set; }
+
+        public List<int> CategoryIds { get; set; }
+
+        public DataTable CategoryIdList { get; set; }
+
+    }
+}
