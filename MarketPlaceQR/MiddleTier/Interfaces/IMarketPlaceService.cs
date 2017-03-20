d8b3d8f24068bd3137f1a6457a82a78cb14dc824
Derrick Crayton <Derrick Crayton>
2017-03-20 08:40:10 -0700

    MarketPlaceQR Interface

---
+++ MarketPlaceQR/MiddleTier/Interfaces/IMarketPlaceService.cs
@@ -0,0 +1,18 @@
+﻿using Sabio.Web.Domain;
+using Sabio.Web.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Services.Interfaces
+{
+    public interface IMarketPlaceService
+    {
+
+        List<MarketPlaceDomain> GetByQuoteRequestId(MarketPlaceRequest model);
+
+        List<MarketPlaceDomain> GetBySingleQuoteRequestId(MarketPlaceRequest model);
+
+    }
+}
